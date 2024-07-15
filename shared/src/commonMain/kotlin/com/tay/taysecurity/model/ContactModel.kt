7e1e3e1bfd27cc43ab920ac1ac31d0b47c308e8b
tayler <emontesinos812@gmail.com>
2024-07-15 13:35:32 -0500

    config queries

--- shared/src/commonMain/kotlin/com/tay/taysecurity/model/ContactModel.kt
+++ shared/src/commonMain/kotlin/com/tay/taysecurity/model/ContactModel.kt
@@ -1,7 +1,7 @@
 package com.tay.taysecurity.model
 
 class ContactModel  (
+    var id :Long = 0,
     var name :String = "",
-    var phoneNumber :String = "",
-    var initialLetter: String = ""
+    var phoneNumber :String = ""
 )
--- shared/src/commonMain/kotlin/com/tay/taysecurity/repository/ContactDataBase.kt
+++ shared/src/commonMain/kotlin/com/tay/taysecurity/repository/ContactDataBase.kt
@@ -3,20 +3,29 @@ package com.tay.taysecurity.repository
 import com.tay.taysecurity.manager.databaseDriverFactory
 import com.tay.taysecurity.model.ContactModel
 import com.tay.taysecurity.usecases.repository.IContactDataBase
+import database.ContactEntity
 import database.ContactSureQueries
 
 
-class ContactDataBase(private val queries:ContactSureQueries = databaseDriverFactory().contactSureQueries): IContactDataBase {
+class ContactDataBase(private val queries:ContactSureQueries = databaseDriverFactory().contactSureQueries)
+    : IContactDataBase {
 
     override suspend fun insertContact(user: ContactModel): Boolean {
-    return true
+        queries.insertContact(user.name, user.phoneNumber)
+        return true
     }
 
     override suspend fun getContactAll(): List<ContactModel> {
-        return arrayListOf()
+        return queries.selectAllContact().executeAsList().toListContact()
     }
 
     override suspend fun deleteContactAll() {
+        queries.deleteContactAll()
+    }
+}
 
+fun List<ContactEntity>.toListContact(): List<ContactModel> {
+    return this.map {
+        ContactModel(it.id,it.name,it.number)
     }
 }
