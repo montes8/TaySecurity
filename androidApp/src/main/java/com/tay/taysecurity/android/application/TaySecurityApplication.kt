6e5b10c734b10dda6618e390cefa94a24fe747f8
tayler <emontesinos812@gmail.com>
2024-07-19 15:59:11 -0500

    add fun delete sms

--- androidApp/src/main/java/com/tay/taysecurity/android/application/TaySecurityApplication.kt
+++ androidApp/src/main/java/com/tay/taysecurity/android/application/TaySecurityApplication.kt
@@ -1,14 +1,24 @@
 package com.tay.taysecurity.android.application
 
 import android.app.Application
-import com.tay.taysecurity.manager.appContext
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
 class TaySecurityApplication : Application(){
+
+    companion object {
+        lateinit  var appContext: Context
+    }
+
+
     override fun onCreate() {
         super.onCreate()
         appContext = this
+        TaySecurityApplication.appContext = applicationContext
     }
+
+
+
 }
---
+++ androidApp/src/main/java/com/tay/taysecurity/android/utils/services/TaySureSmsBroadcast.kt
@@ -0,0 +1,57 @@
+package com.tay.taysecurity.android.utils.services
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
+import android.os.Handler
+import android.provider.Telephony
+import android.util.Log
+import com.tay.taysecurity.android.application.TaySecurityApplication.Companion.appContext
+
+
+class TaySureSmsBroadcast: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, intent: Intent?) {
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION == intent?.action) {
+            for (smsMessage in Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                val messageBody = smsMessage.messageBody
+
+                Log.d("TAGTay","$messageBody")
+                Log.d("TAGTay","${smsMessage.originatingAddress}")
+                Handler().postDelayed({
+                    delete(smsMessage.originatingAddress?:"")
+                },1000)
+
+            }
+            Log.d("TAGTay","messageBody")
+        }
+
+
+    }
+
+    fun delete(thread: String) {
+        val c: Cursor = appContext.contentResolver.query(
+            Uri.parse("content://sms/"),
+            arrayOf<String>("_id", "thread_id", "address", "person", "date", "body"),
+            null,
+            null,
+            null
+        )!!
+        Log.d("TAGTay","delete")
+        try {
+            while (c.moveToNext()) {
+                val id = c.getInt(0)
+                val address = c.getString(2)
+                if (address == thread) {
+                    appContext.contentResolver.delete(
+                        Uri.parse("content://sms/$id"), null, null
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.d("TAGTay","Exception")
+        }
+    }
+}
