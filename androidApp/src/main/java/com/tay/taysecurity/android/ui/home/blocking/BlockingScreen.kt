395860fa7c50c92e0cab091321581a788f7a1976
tayler <emontesinos812@gmail.com>
2024-07-28 13:01:59 -0500

    test component v1

--- androidApp/src/main/java/com/tay/taysecurity/android/ui/home/blocking/BlockingScreen.kt
+++ androidApp/src/main/java/com/tay/taysecurity/android/ui/home/blocking/BlockingScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.tay.taysecurity.android.R
+import com.tay.taysecurity.android.component.TayCardItem
 import com.tay.taysecurity.android.ui.blocking.BlockingViewModel
 
 @Composable
@@ -50,120 +51,21 @@ fun BlockingScreen(
         )
 
         Spacer(modifier = Modifier.height(16.dp))
-        Card(shape = RoundedCornerShape(8.dp),
-            backgroundColor = Color.Black,elevation = 4.dp) {
-            Row{
-                Column(modifier = Modifier
-                    .weight(4.0f)
-                    .padding(16.dp)) {
-                    Text(
-                        text = "Bloqueo de Todas  las llamadas ",
-                        fontSize = 18.sp,
-                        fontFamily = FontFamily(Font(R.font.ui_tay_montserrat_semi_bold)),
-                        color = Color.White
-                    )
-                    Text(
-                        modifier = Modifier.padding( top = 2.dp),
-                        text = "Esta opción bloqueara las llamadas entrantes a tu telefono movil.",
-                        fontSize = 12.sp,
-                        fontFamily = FontFamily(Font(R.font.ui_tay_montserrat_medium)),
-                        color = Color.White
-                    )
-                }
-                Switch(
-                    modifier = Modifier.align(Alignment.CenterVertically),
-                    checked = viewModel.uiState.securty.blockingCallFull,
-                    onCheckedChange = {
-                        viewModel.uiState.securty.blockingCallFull = it
-                        if(viewModel.uiState.securty.blockingCallFull){
-                            //checkedCall.value = true
-                        }
-                    },colors = SwitchDefaults.colors(
-                        checkedThumbColor = Color.Magenta,
-                        checkedTrackColor = Color.White,
-                        uncheckedThumbColor =  Color.Magenta,
-                        uncheckedTrackColor = Color.White,
-                    )
-
-                )
-            }
+        TayCardItem(state =viewModel.uiState.securty.blockingCallFull,
+            text = "Bloqueo de Todas  las llamadas.",
+            subText = "Esta opción bloqueara las llamadas entrantes a tu telefono movil."){
         }
 
         Spacer(modifier = Modifier.height(16.dp))
-        Card(shape = RoundedCornerShape(8.dp),
-            backgroundColor = Color.Black,elevation = 4.dp) {
-            Row{
-                Column(modifier = Modifier
-                    .weight(4.0f)
-                    .padding(16.dp)) {
-                    Text(
-                        text = "Bloqueo de llamadas desconocidas",
-                        fontSize = 18.sp,
-                        fontFamily = FontFamily(Font(R.font.ui_tay_montserrat_semi_bold)),
-                        color = Color.White
-                    )
-                    Text(
-                        modifier = Modifier.padding( top = 2.dp),
-                        text = "Esta opción bloqueara total las llamadas entrantes que no esten registrados en tus contactos.",
-                        fontSize = 12.sp,
-                        fontFamily = FontFamily(Font(R.font.ui_tay_montserrat_medium)),
-                        color = Color.White
-                    )
-                }
-                Switch(
-                    modifier = Modifier.align(Alignment.CenterVertically),
-                    checked = viewModel.uiState.securty.blockingCall,
-                    onCheckedChange = {
-                        viewModel.uiState.securty.blockingCall = it
-                    },colors = SwitchDefaults.colors(
-                        checkedThumbColor = Color.Magenta,
-                        checkedTrackColor = Color.White,
-                        uncheckedThumbColor =  Color.Magenta,
-                        uncheckedTrackColor = Color.White,
-                    )
-
-                )
-            }
+        TayCardItem(state =viewModel.uiState.securty.blockingCall,
+            text = "Bloqueo de llamadas desconocidas",
+            subText = "Esta opción bloqueara total las llamadas entrantes que no esten registrados en tus contactos."){
         }
 
         Spacer(modifier = Modifier.height(16.dp))
-        Card(shape = RoundedCornerShape(8.dp),
-            backgroundColor = Color.Black,elevation = 4.dp) {
-            Row{
-                Column(modifier = Modifier
-                    .weight(4.0f)
-                    .padding(16.dp)) {
-                    Text(
-                        text = "Bloqueo de todos los mensajes entrantes",
-                        fontSize = 18.sp,
-                        fontFamily = FontFamily(Font(R.font.ui_tay_montserrat_semi_bold)),
-                        color = Color.White
-                    )
-                    Text(
-                        modifier = Modifier.padding( top = 2.dp),
-                        text = "Esta opción bloqueara y eliminara todos los mensaje entrantes en tu bandeja de entrada.",
-                        fontSize = 12.sp,
-                        fontFamily = FontFamily(Font(R.font.ui_tay_montserrat_medium)),
-                        color = Color.White
-                    )
-                }
-                Switch(
-                    modifier = Modifier.align(Alignment.CenterVertically),
-                    checked = viewModel.uiState.securty.blockingSmsFull,
-                    onCheckedChange = {
-                        viewModel.uiState.securty.blockingSmsFull = it
-                       // if(checkedMessageTotal.value){
-                      //      checkedMessage.value = true
-                      //  }
-                    },colors = SwitchDefaults.colors(
-                        checkedThumbColor = Color.Magenta,
-                        checkedTrackColor = Color.White,
-                        uncheckedThumbColor =  Color.Magenta,
-                        uncheckedTrackColor = Color.White,
-                    )
-
-                )
-            }
+        TayCardItem(state =viewModel.uiState.securty.blockingSmsFull,
+            text = "Bloqueo de todos los mensajes entrantes",
+            subText = "Esta opción bloqueara y eliminara todos los mensaje entrantes en tu bandeja de entrada."){
         }
 
         Spacer(modifier = Modifier.height(16.dp))
