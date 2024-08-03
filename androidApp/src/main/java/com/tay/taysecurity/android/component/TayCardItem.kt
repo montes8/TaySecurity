e5b54ffb70d13c5db9c35bbb99b27714727ff96c
tayler <emontesinos812@gmail.com>
2024-08-03 11:52:14 -0500

    remove  click image v2

--- androidApp/src/main/java/com/tay/taysecurity/android/component/TayCardItem.kt
+++ androidApp/src/main/java/com/tay/taysecurity/android/component/TayCardItem.kt
@@ -1,56 +1,38 @@
 package com.tay.taysecurity.android.component
 
-import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Text
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tay.taysecurity.android.R
-import kotlinx.coroutines.launch
-import kotlin.math.roundToInt
 
 
 @Composable
@@ -99,9 +81,13 @@ fun TayCardItem(
 
             ) {
                 Image(
-                    modifier = Modifier.height(28.dp)
-                        .width(28.dp).padding(2.dp),
-                    imageVector = ImageVector.vectorResource(R.drawable.tay_circle_white),
+                    painter = painterResource(R.drawable.tay_circle_white),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(28.dp).padding(2.dp)
+                        .clip(CircleShape)                       // clip to the circle shape
+                        .border(2.dp, Color.Black, CircleShape)
+                        .background(Color.Magenta) ,
                     contentDescription = "",
                     alignment = Alignment.Center
                 )
@@ -111,16 +97,6 @@ fun TayCardItem(
 
 }
 
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun CustomSwitch(
-    height: Dp,
-    width: Dp,
-    state: Int,
-    onCheckedChanged: (Boolean) -> Unit
-) {
-
-}
 
 @Composable
 fun Modifier.uiTayNoRippleClickable(
