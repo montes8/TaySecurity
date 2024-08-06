aa218fb79aa86e4db7d6c802c619d8cbf51fe959
tayler <emontesinos812@gmail.com>
2024-08-06 15:08:12 -0500

    create view screen security

--- androidApp/src/main/java/com/tay/taysecurity/android/ui/home/security/SecurityScreen.kt
+++ androidApp/src/main/java/com/tay/taysecurity/android/ui/home/security/SecurityScreen.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -16,34 +20,43 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.tay.taysecurity.android.R
+import com.tay.taysecurity.android.component.TayCardItem
 
 @Composable
 fun SecurityScreen(
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.SpaceAround,
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+            .padding(top= 8.dp, start = 8.dp, end = 8.dp, bottom = 78.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "text",
-            style = TextStyle(color = Color.Black, fontSize = 42.sp, fontWeight = FontWeight.Black)
+            text = "Configuración de \nbloqueos",
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            fontFamily = FontFamily(Font(R.font.ui_tay_montserrat_bold))
         )
 
-        Button(
-            onClick = { }
-        ) {
-            Icon(
-                imageVector = Icons.Default.DarkMode,
-                contentDescription = "Dark mode Icon"
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text("Toggle Dark Mode")
+        Spacer(modifier = Modifier.height(16.dp))
+        TayCardItem(state =true,
+            text = "Bloqueo de capturas y grabacion de pantalla",
+            subText ="Esta opción no permite tomar capturas ni grabar la pantalla en este dispositivo."){
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        TayCardItem(state =true,
+            text = "Bloqueo de grabacion de audio",
+            subText ="Esta opción no permite grabar audios."){
+
         }
     }
 }
