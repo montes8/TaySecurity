9f79797fc8e0ec416d89aabeddb5725c42c608bd
tayler <emontesinos812@gmail.com>
2024-08-12 20:52:38 -0500

    clean v2

--- androidApp/src/main/java/com/tay/taysecurity/android/utils/services/MmsReceiver.kt
+++ androidApp/src/main/java/com/tay/taysecurity/android/utils/services/MmsReceiver.kt
@@ -6,6 +6,6 @@ import android.content.Intent
 
 class MmsReceiver : BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
-        TODO("Not yet implemented")
+        //not implementation
     }
 }
