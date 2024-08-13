be692ecc01fad7d682b984f3d58132f529e1fa29
tayler <emontesinos812@gmail.com>
2024-08-12 20:53:45 -0500

    clean v3

--- androidApp/src/main/java/com/tay/taysecurity/android/utils/services/MmsReceiver.kt
+++ androidApp/src/main/java/com/tay/taysecurity/android/utils/services/MmsReceiver.kt
@@ -5,7 +5,14 @@ import android.content.Context
 import android.content.Intent
 
 class MmsReceiver : BroadcastReceiver() {
-    override fun onReceive(p0: Context?, p1: Intent?) {
-        //not implementation
+    override fun onReceive(p0: Context?, intent: Intent?) {
+        when (intent?.action) {
+            Intent.ACTION_DATE_CHANGED -> {
+                //not implementation
+            }
+            Intent.ACTION_BOOT_COMPLETED -> {
+                //not implementation
+            }
+        }
     }
 }
