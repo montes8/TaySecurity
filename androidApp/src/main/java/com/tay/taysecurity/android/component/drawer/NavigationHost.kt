3078ac0cbc0489c890dbe92e113b5ac7211ffaf2
tayler <emontesinos812@gmail.com>
2024-08-13 19:01:24 -0500

    delete

--- androidApp/src/main/java/com/tay/taysecurity/android/component/drawer/NavigationHost.kt
+++ androidApp/src/main/java/com/tay/taysecurity/android/component/drawer/NavigationHost.kt
@@ -1,10 +1,9 @@
-package com.dev.leonardom.introuduccionajetpackcompose.navigation
+package com.tay.taysecurity.android.component.drawer
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.tay.taysecurity.android.component.drawer.Destinations
 import com.tay.taysecurity.android.ui.home.blocking.BlockingScreen
 import com.tay.taysecurity.android.ui.home.extra.ExtraScreen
 import com.tay.taysecurity.android.ui.home.security.SecurityScreen
@@ -14,18 +13,8 @@ fun NavigationHost(
     navController: NavHostController
 ) {
     NavHost(navController = navController, startDestination = Destinations.BlockingScreen.route) {
-        composable(Destinations.BlockingScreen.route) {
-            BlockingScreen()
-        }
-
-        composable(
-            Destinations.SecurityScreen.route
-        ) {
-            SecurityScreen()
-        }
-
-        composable(Destinations.ExtraScreen.route) {
-            ExtraScreen()
-        }
+        composable(Destinations.BlockingScreen.route) { BlockingScreen()}
+        composable(Destinations.SecurityScreen.route) { SecurityScreen()}
+        composable(Destinations.ExtraScreen.route) {ExtraScreen() }
     }
 }
