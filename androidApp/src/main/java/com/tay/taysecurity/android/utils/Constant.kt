cc0403dd7ab2265f7305bf16f717fb06452ab31d
Tayler <emontesinos812@gmail.com>
2025-07-28 14:28:43 -0500

    update text

--- androidApp/src/main/java/com/tay/taysecurity/android/utils/Constant.kt
+++ androidApp/src/main/java/com/tay/taysecurity/android/utils/Constant.kt
@@ -13,4 +13,5 @@ const  val messageBlockingGps = "Para utilizar esta funcionalidad, lo primero qu
         "Luego debes activar poner como predeterminado la aplicacion como predeterminado:\n-Configutación.\n-Sistema del telefono(similares).\n--Opciones de desarrollador.\n-Selecciona aplicación de ubicacion.(similares)"
 const  val titleBlockingImage = "INFORMACIÓN DE IMAGEN"
 const  val messageBlockingImage = "Para utilizar esta funcionalidad, lo primero que se debe hacer es activar el permiso de escritura y lectura para poder acceder a alguna imagen:\n-Configuración.\n-Aplicaciones.\n-Selecciona la aplicación\n-Permisos.\nActivar permiso de almacenamiento.(similares)" +
-        "Luego debes activar los permisos relacionados a llamadas:\n-Configutación.\n-Aplicaciones.\n-Seleciona ValuSure.\n-Permisos.\n-Activa los permisos de mensaje de texto."
+        "Luego debes activar los permisos relacionados a llamadas:\n-Configutación.\n-Aplicaciones.\n-Seleciona ValuSure.\n-Permisos.\n-Activa los permisos de mensaje de texto.\nDe preferencia la imagen debe estar en las carpetas principales como, descargas, fotos," +
+        " o camara "
