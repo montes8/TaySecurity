516d23eba49339fe3fa8a4006c022114127334a8
Tayler <emontesinos812@gmail.com>
2025-08-18 06:46:05 -0500

    update version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.tay.taysecurity.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
      signingConfigs {
         create("release") {
